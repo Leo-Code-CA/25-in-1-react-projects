8002f5486b43d62929749bab385ca1bd7ebb1dcc
Leo-Code-CA <leonore.maciuga.ca@gmail.com>
2024-03-14 19:35:13 -0400

    regex problem fixed, color component almost done

--- src/components/color-generator/index.jsx
+++ src/components/color-generator/index.jsx
@@ -26,36 +26,36 @@ export default function ColorGenerator() {
             setColor(`rgb(${r}, ${g}, ${b})`);
         }
 
-        function handleRgbToHex(r, g, b) {
-            const buildHex = [r, g, b].map(x => {
-                const hex = x.toString(16);
+        // function handleRgbToHex(r, g, b) {
+        //     const buildHex = [r, g, b].map(x => {
+        //         const hex = x.toString(16);
+        //         return hex.length === 1 ? `0${hex}` : hex;
+        //     });
+        //     return `#${buildHex.join('')}`;
+        // }
+
+        function handleRgbToHex(rgb) {
+
+            const extractColor = rgb?.match(/(\d{1,3})/g);
+            const buildHex = extractColor.map(x => {
+                const hex = Number(x).toString(16);
                 return hex.length === 1 ? `0${hex}` : hex;
             });
             return `#${buildHex.join('')}`;
         }
 
         function handleHextoRbg(hex) {
-
-            const regexPattern = /^#([a-f\d])([a-f\d])([a-f\d])/i;
-
-            return hex
-            .replace(regexPattern, `$1`); 
-
-
-//                 const hexToRgb = hex =>
-//   hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-//              ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-//     .substring(1).match(/.{2}/g)
-//     .map(x => parseInt(x, 16))
-
-
+            const buildRgb = hex
+                .slice(1).match(/.{2}/g)
+                .map(x => parseInt(x, 16))
+                .join(',');
+            return `rgb(${buildRgb})`;
         }
-
-        const paragraph = "#bb55df";
-        console.log(handleHextoRbg(paragraph));
 
         function handleSwitch() {
             setTypeOfColor(c => c === 'hex' ? 'rgb' : 'hex');
+            typeOfColor === 'hex' ? setColor(handleHextoRbg(color))
+            : setColor(handleRgbToHex(color));
         }
     
     return (
@@ -71,7 +71,7 @@ export default function ColorGenerator() {
                             left: typeOfColor === 'hex' ? '0' : '100%',
                             transform: typeOfColor === 'hex' ? 'translate(0)' : 'translate(-100%)'
                         }}
-                        onClick={() => setTypeOfColor(c => c === 'hex' ? 'rgb' : 'hex')}
+                        onClick={handleSwitch}
                         ></button>
                     </div>
                     <p>RGB</p>
@@ -83,8 +83,8 @@ export default function ColorGenerator() {
                 </button>
             </div>
             <div className="colorGenerator__color" style={{background: color}}>
-                <h3>{typeOfColor === 'rgb' ? 'RGB Color' : 'HEX Color'}</h3>
-                <h1>{color}</h1>
+                <h4>{typeOfColor === 'rgb' ? 'RGB Color' : 'HEX Color'}</h4>
+                <h3>{color}</h3>
             </div>
         </div>
     );
