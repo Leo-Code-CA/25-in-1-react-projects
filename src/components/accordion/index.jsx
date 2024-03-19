d858a6dcc2e1d5eaba2a2967e28eea1345d87f7f
Leo-Code-CA <leonore.maciuga.ca@gmail.com>
2024-03-19 14:49:38 -0400

    accordion mostly done and qr code component ongoing:

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,8 +1,60 @@
-
+import { useState } from "react";
+import data from './data.js';
+import { FaSquarePlus, FaSquareMinus } from "react-icons/fa6";
+import './style.css';
 
 export default function Accordion() {
 
+    const [selected, setSelected] = useState([]);
+    const [enableMultipleSelection, setEnableMultipleSelection] = useState(false);
+
+    function handleSingleSelection(getCurrentId) {
+        setSelected(selected[0] === getCurrentId ? [] : [getCurrentId]);
+    }
+
+    function handleMultiSelection(getCurrentId) {
+
+        const findIndexOfCurrentId = selected.indexOf(getCurrentId);
+
+        findIndexOfCurrentId === -1 ? setSelected([...selected, getCurrentId])
+        : setSelected(selected.slice(0, findIndexOfCurrentId).concat(selected.slice(findIndexOfCurrentId + 1)));
+
+    }
+
     return (
-        <div>Accordion Component</div>
+        <div className="accordion">
+            <button
+            className="accordion__settings"
+            onClick={() => setEnableMultipleSelection(!enableMultipleSelection)}>
+                {enableMultipleSelection ? 'Disable' : 'Enable'} Multiple Selection
+            </button>
+            <div className="accordion__content">
+                {
+                    data && data.length > 0 ? 
+                    data.map(dataItem =>
+                        <dl className="accordion__item" key={dataItem.id}>
+                            <dt
+                                className="accordion__itemTitle"
+                                onClick={enableMultipleSelection 
+                                ? () => handleMultiSelection(dataItem.id) 
+                                : () => handleSingleSelection(dataItem.id)}>
+                                <h3>{dataItem.question}</h3>
+                                <span>
+                                    {
+                                        selected.indexOf(dataItem.id) !== -1 ? <FaSquareMinus /> : <FaSquarePlus />
+                                    }
+                                </span>
+                            </dt>
+                            {
+                                enableMultipleSelection ?
+                                selected.indexOf(dataItem.id) !== -1 && <dd className="accordion__itemData">{dataItem.answer}</dd>
+                                : selected[0] === dataItem.id && <dd className="accordion__itemData">{dataItem.answer}</dd>
+                            }
+                        </dl>
+                    )
+                    : <div>No data found!</div>
+                }
+            </div>
+        </div>
     );
 }
--- src/components/qr-code-generator/index.jsx
+++ src/components/qr-code-generator/index.jsx
@@ -1,8 +1,45 @@
-
+import { useState } from "react";
+import QRCode from "react-qr-code";
+import './style.css';
 
 export default function QrCodeGenerator() {
 
+    const [qrCodeValue, setQrCodeValue] = useState("");
+    const [inputValue, setInputValue] = useState("");
+
+    function handleGenerateQrCode() {
+        setQrCodeValue(inputValue);
+        setInputValue("");
+    }
+
     return (
-        <div>QR Code Generator Component</div>
+        <div className="qrcode">
+            <h2>QR Code Generator</h2>
+            <div className="qrcode__input">
+                <label htmlFor="qrcode-input">Enter your message below:</label>
+                <input
+                id="qrcode-input"
+                onChange={(e) => setInputValue(e.target.value)}
+                type="text" 
+                name="qrcode-input" 
+                placeholder="Enter your message here"
+                value={inputValue}/>
+                <button
+                onClick={handleGenerateQrCode}
+                disabled={inputValue && inputValue.trim() !== '' ? false : true}>
+                    Generate
+                </button>
+            </div>
+            <div className="qrcode__result">
+                <QRCode 
+                title="qrcode"
+                value={qrCodeValue}
+                bgColor="#fff"
+                />
+            </div>
+            <a href="#" download='/test.png'>Download</a>
+        </div>
     );
 }
+
+// check that: https://gist.github.com/tatsuyasusukida/1261585e3422da5645a1cbb9cf8813d6
